e2446e56783bc8fee5dc79591a5c798c111e6461
Jesus-170503 <104458573+Jesus-170503@users.noreply.github.com>
2022-08-24 09:49:52 -0600

    Add files via upload

---
+++ EVA1_5_FOR_EACH/src/eva1_5_for_each/EVA1_5_FOR_EACH.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_5_for_each;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA1_5_FOR_EACH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic
+        
+        int[] arregloDatos = new int[50];
+        for (int i = 0; i < arregloDatos.length; i++) {
+            arregloDatos[i]= (int)(Math.random() * 100) ;
+            
+            
+        }
+        //for-each
+        for(int arregloDato : arregloDatos){
+            System.out.print("[" + arregloDato + "]");
+        }
+    }
+    
+}
