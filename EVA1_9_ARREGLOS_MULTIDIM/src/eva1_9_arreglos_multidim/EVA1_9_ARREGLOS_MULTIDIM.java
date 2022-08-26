1ca32cf22cbad6fe3965b0cd7468d7a9744d0873
Jesus-170503 <104458573+Jesus-170503@users.noreply.github.com>
2022-08-26 09:51:45 -0600

    Add files via upload

---
+++ EVA1_9_ARREGLOS_MULTIDIM/src/eva1_9_arreglos_multidim/EVA1_9_ARREGLOS_MULTIDIM.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_arreglos_multidim;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA1_9_ARREGLOS_MULTIDIM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int [][] matrizDatos = new int[3][5];
+        System.out.println("filas"+matrizDatos.length);
+        for (int i = 0; i < matrizDatos.length; i++) { // primer dimension (3 filas)
+            System.out.println("Columnas"+ matrizDatos[i].length);
+            for (int j = 0; j < matrizDatos[i].length; j++) { // segunda dimension (5 columnas)
+                matrizDatos[i][j] = 100;
+            } 
+        }
+  
+        for (int i = 0; i < matrizDatos.length; i++) { // primer dimension (3 filas)
+            for (int j = 0; j < matrizDatos[i].length; j++) { // segunda dimension (5 columnas)
+                matrizDatos[i][j] = 100;
+                System.out.print("["+matrizDatos[i][j] + "]");
+            }
+             System.out.println("");
+        }
+        
+       
+    }
+    
+}
