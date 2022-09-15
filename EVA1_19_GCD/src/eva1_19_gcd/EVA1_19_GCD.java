9c10521dc07f25f897169cd7bf9417178bed35ed
Jesus-170503 <104458573+Jesus-170503@users.noreply.github.com>
2022-09-15 09:53:56 -0600

    Add files via upload

---
+++ EVA1_19_GCD/src/eva1_19_gcd/EVA1_19_GCD.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_19_gcd;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA1_19_GCD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(gcd(181, 7));
+        
+        
+    }
+    
+    public static int gcd(int dividendo, int divisor){
+        
+        int residuo;
+        residuo =  dividendo%divisor;
+        
+        if (residuo == 0){//me deteng , encontre el mcd
+            return divisor;
+        }else{
+          return gcd(divisor, residuo);
+        }
+        
+    }
+    
+}
