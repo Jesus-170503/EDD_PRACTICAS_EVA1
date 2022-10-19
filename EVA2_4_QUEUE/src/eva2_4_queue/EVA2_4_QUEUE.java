03763990456e256b2c34c6f782da7370391f4074
Jesus-170503 <104458573+Jesus-170503@users.noreply.github.com>
2022-10-19 09:52:53 -0600

    Add files via upload

---
+++ EVA2_4_QUEUE/src/eva2_4_queue/EVA2_4_QUEUE.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_4_queue;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA2_4_QUEUE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        
+    }
+    
+}
